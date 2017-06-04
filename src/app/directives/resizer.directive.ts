68c7a331a9dbf9f39642d8723049318ca9b6709f
aniruddha <aniruddha.kudalkar@gmail.com>
2017-06-04 14:53:21 +0530

    directives: input to the directives

--- src/app/directives/resizer.directive.ts
+++ src/app/directives/resizer.directive.ts
@@ -1,20 +1,33 @@
-import { Directive, ElementRef, Renderer2, Renderer, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  Renderer,
+  HostListener,
+  HostBinding,
+  Input
+} from '@angular/core';
 
 @Directive({
   selector: '[resizer]'
 })
 export class ResizerDirective {
 
+  @HostBinding('style.width') @Input('resizer') width: string;
+  @Input() maxWidth: string;
+
   constructor(private elRef: ElementRef, private rend: Renderer2) {
     //this.elRef.nativeElement.style.width = '300px';
-    this.rend.setStyle(this.elRef.nativeElement, 'width', '300px');
+    //this.rend.setStyle(this.elRef.nativeElement, 'width', '300px');
   }
 
   @HostListener('mouseenter') bigger() {
-    this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
+    //this.rend.setStyle(this.elRef.nativeElement, 'width', '500px');
+    this.width = '500px';
   }
 
   @HostListener('mouseleave') smaller() {
-    this.rend.setStyle(this.elRef.nativeElement, 'width', '300px');
+    //this.rend.setStyle(this.elRef.nativeElement, 'width', '300px');
+    this.width = '300px';
   }
 }
